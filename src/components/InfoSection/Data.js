fa15a77d62c8b9847fdb1e27b14995bac41119d1
Bruna Mazzaron <69203877+brunamazzaron@users.noreply.github.com>
2022-11-05 01:42:07 -0300

    projeto fiap

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'The long wait is over',
+  headline: 'New Collection',
+  description:
+  'Writing effectively is an art. Start by using simple, everyday words people can easily understand. Be clear and direct to the point. Keep your thoughts flowing logically, and aim for brevity unless you’re writing in the long form.',
+  description2:
+    'Your ideas have a purpose so choose words that accurately express them. Ensure your grammar is flawless as it impacts your credibility. Use the active voice whenever possible as it makes any narrative easier to read.',
+  buttonLabel: 'Baixe nosso app',
+  imgStart: false,
+  img: require('../../images/mobile.png'),
+  alt: 'Mobile',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description:
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+  buttonLabel: 'Conheça um profissional',
+  imgStart: true,
+  img: require('../../images/psico.png'),
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg'),
+  alt: 'Papers',
+  dark: false,
+  primary: false,
+  darkText: true
+};
